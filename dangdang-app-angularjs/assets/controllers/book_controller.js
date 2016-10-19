a09eddfbec15a422a788605b122d03e187a388fa
QinXZ1122 <email@example.com>
2016-10-19 17:08:49 +0800

    给book列表添加图片

--- dangdang-app-angularjs/assets/controllers/book_controller.js
+++ dangdang-app-angularjs/assets/controllers/book_controller.js
@@ -1,8 +1,8 @@
 app.controller('bookController',['$scope',function($scope){
 $scope.bookTypes = [
-  {id:'ertong',name:'儿童',img:'',description:''},
-  {id:'lishi',name:'历史',img:'',description:''},
-  {id:'dongman',name:'动漫',img:'',description:''},
-  {id:'qingchunwenxue',name:'青春文学',img:'',description:''},
+  {id:'ertong',name:'儿童',img:'http://img4.imgtn.bdimg.com/it/u=925981019,3880874217&fm=21&gp=0.jpg',description:'当当网童书热销榜'},
+  {id:'lishi',name:'历史',img:'http://img5.imgtn.bdimg.com/it/u=1002014815,3067355128&fm=21&gp=0.jpg',description:'当当网历史热销榜'},
+  {id:'dongman',name:'动漫',img:'http://img0.imgtn.bdimg.com/it/u=3116934751,321188681&fm=21&gp=0.jpg',description:'当当网动漫热销榜'},
+  {id:'qingchunwenxue',name:'青春文学',img:'http://img0.imgtn.bdimg.com/it/u=1006888422,3230244852&fm=21&gp=0.jpg',description:'当当网青春文学热销榜'},
 ];
 }]);
