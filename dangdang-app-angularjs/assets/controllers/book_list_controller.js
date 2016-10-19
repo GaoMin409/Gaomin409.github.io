e6490313f6e3814f2be9db5a07bfef22ea01e395
QinXZ1122 <email@example.com>
2016-10-19 21:38:13 +0800

    根据id获取数据

--- dangdang-app-angularjs/assets/controllers/book_list_controller.js
+++ dangdang-app-angularjs/assets/controllers/book_list_controller.js
@@ -1,7 +1,9 @@
 app.controller('bookListController', ['$scope','commonService','$routeParams', function($scope,c_service,$routeParams) {
+  // console.dir($routeParams.name);
   console.dir($routeParams.id);
+  // console.dir($routeParams);
 
-  c_service.getData(c_service.id,function(res){
+c_service.getData(c_service.id,function(res){
       console.dir(res);
     });
 
