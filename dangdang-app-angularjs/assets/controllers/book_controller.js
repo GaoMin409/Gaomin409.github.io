06f989398a0eebc509358c98356a379b99f5797e
QinXZ1122 <email@example.com>
2016-10-19 17:20:05 +0800

    修改图片路径及大小

--- dangdang-app-angularjs/assets/controllers/book_controller.js
+++ dangdang-app-angularjs/assets/controllers/book_controller.js
@@ -1,8 +1,8 @@
 app.controller('bookController',['$scope',function($scope){
 $scope.bookTypes = [
-  {id:'ertong',name:'儿童',img:'./assets/images/bok_list/ertong.jpg',description:'当当网童书热销榜'},
-  {id:'lishi',name:'历史',img:'./assets/images/bok_list/lishi.jpg',description:'当当网历史热销榜'},
-  {id:'dongman',name:'动漫',img:'./assets/images/bok_list/dongman.jpg',description:'当当网动漫热销榜'},
-  {id:'qingchunwenxue',name:'青春文学',img:'./assets/images/bok_list/qingchunwenxue.jpg',description:'当当网青春文学热销榜'},
+  {id:'ertong',name:'儿童',img:'./assets/images/book_list/ertong.jpg',description:'当当网童书热销榜'},
+  {id:'lishi',name:'历史',img:'./assets/images/book_list/lishi.jpg',description:'当当网历史热销榜'},
+  {id:'dongman',name:'动漫',img:'./assets/images/book_list/dongman.jpg',description:'当当网动漫热销榜'},
+  {id:'qingchunwenxue',name:'青春文学',img:'./assets/images/book_list/qingchunwenxue.jpg',description:'当当网青春文学热销榜'},
 ];
 }]);
