2b6a2f1df612d4f641c26a49f15fb09051e9c769
QinXZ1122 <email@example.com>
2016-10-19 21:53:10 +0800

    ---------

--- dangdang-app-angularjs/assets/controllers/book_list_controller.js
+++ dangdang-app-angularjs/assets/controllers/book_list_controller.js
@@ -1,9 +1,10 @@
-app.controller('bookListController', ['$scope','commonService','$routeParams', function($scope,c_service,$routeParams) {
+app.controller('bookListController', ['$scope','commonService','$routeParams', function($scope,c_s,$routeParams) {
   // console.dir($routeParams.name);
   console.dir($routeParams.id);
   // console.dir($routeParams);
 
-c_service.getData($routeParams.id,function(res){
+c_s.getData($routeParams.id,function(res){
+  var bookData = res.data;
       console.dir(res.data);
     });
 
