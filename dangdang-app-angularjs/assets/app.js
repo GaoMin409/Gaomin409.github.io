fb5ad5e4945b2d5f63727f9e912c3bf116c45cdc
QinXZ1122 <email@example.com>
2016-10-19 16:50:56 +0800

    当当网界面-数据展示模板

--- dangdang-app-angularjs/assets/app.js
+++ dangdang-app-angularjs/assets/app.js
@@ -1,1 +1,23 @@
-var app = angular.module('app',['ngRoute']);
+var app = angular.module('app', ['ngRoute']);
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: './tpl/index.html',
+            controller: ''
+        })
+        .when('/blog', {
+            templateUrl: './tpl/blog.html',
+            controller: ''
+        })
+        .when('/book', {
+            templateUrl: './tpl/book.html',
+            controller: 'bookController'
+        })
+        .when('/about_me', {
+            templateUrl: './tpl/about_me.html',
+            controller: ''
+        }).otherwise({
+            redirectTo: '/'
+        });
+
+}]);
--- dangdang-app-angularjs/assets/controllers/main_controller.js
+++ dangdang-app-angularjs/assets/controllers/main_controller.js
@@ -1,11 +1,12 @@
 app.controller('mainController', ['$scope', 'commonService', function($scope, c_service) {
     // 参数一  type 分类信息
-    c_service.getData('ertong', function(res) {
-        console.dir(res);
-    });
-}]);
+//     c_service.getData('ertong', function(res) {
+//         console.dir(res);
+//     });
+// }]);
 
 $scope.selectedIndex = 0;
 $scope.linkTo = function(index) {
     $scope.selectedIndex = index;
 };
+}]);
